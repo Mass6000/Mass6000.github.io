b744b1935b21f7edcec5743640f7c56551ba272c
John Chase <john.chase@live.com>
2019-04-08 06:12:34 -0600

    Various changes

--- control.js
+++ control.js
@@ -1,6 +1,7 @@
-let courseHoles = [];
+const courseHoles = [],
+    testPlayer = [];
 
-// THIS SECTION GETS THE API DATA FROM THE SOURCE XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+// THIS SECTION GETS THE API DATA FROM THE SOURCE XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 //
 //     https://golf-courses-api.herokuapp.com/courses
 //       Fox Hollow id: 18300
@@ -9,7 +10,7 @@ let courseHoles = [];
 //
 //     https://golf-courses-api.herokuapp.com/courses/${courseid}
 //       This last URL contains all the information required for the selected course
-//
+// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
 function getGolfCourses() {
     let allGolfCourses;
@@ -25,33 +26,28 @@ function getGolfCourses() {
 }
 
 function drawGolfCourses(allGolfCourses) {
-    $('.containerCourse').append(`<h1>Course Selections Page</h1>`);
-    $('.containerCourse').append(`<h4>Choose a course from the dropdown</h4>`);
+    $('.containerCourse').append(`<h1>Available Courses</h1>`);
     $('.containerCourse').append(`<select name="golfCourses" id="golfCourses"></select>`);
     $('.containerCourse').append(`<noscript><input type="submit" value="Submit"></noscript>`);
     $('.containerCourse').append(`<div id="courseImage"></div>`);
     $('.containerCourse').append(`<button id="courseSubmit" type="button"></button>`); // This button is replace below
-    // as I could not figure out a way to keep it below the image without setting it here. XXXXXXXXXXXXXXXXXXXXXXXXXXX
-
-
-
+    // as I could not figure out a way to keep it below the image without setting it here. XXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
     for (let i = 0; i < allGolfCourses.courses.length; i++) {
         if (i === 0) {
             $('#golfCourses').append(`<option value="${allGolfCourses.courses[i].id}" id="${allGolfCourses.courses[i].id}">${allGolfCourses.courses[i].name}</option>`);
             $('#courseImage').append(`<img id="imageCourse" src="${allGolfCourses.courses[i].image}" alt="Image of the ${allGolfCourses.courses[i].name}">`);
-            $('#courseSubmit').replaceWith(`<button id="courseSubmit" onclick="getGolfCourse(${allGolfCourses.courses[i].id})">Complete your selection by pressing here</button>`)
+            $('#courseSubmit').replaceWith(`<button id="courseSubmit" onclick="getGolfCourse(${allGolfCourses.courses[i].id})">Choose Your Course</button>`)
         } else {
             $('#golfCourses').append(`<option value="${allGolfCourses.courses[i].id}" id="${allGolfCourses.courses[i].id}">${allGolfCourses.courses[i].name}</option>`);
         }
     }
-
     // This is where the individual course is selected XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
     $('#golfCourses').change(function () {
         for (let i = 0; i < allGolfCourses.courses.length; i++) {
             if ($(this).val() == allGolfCourses.courses[i].id) {
                 $('#imageCourse').replaceWith(`<img id="imageCourse" src="${allGolfCourses.courses[i].image}" alt="Image of the ${allGolfCourses.courses[i].name}">`);
-                $('#courseSubmit').replaceWith(`<button id="courseSubmit" onclick="getGolfCourse(${allGolfCourses.courses[i].id})">Submit</button>`);
+                $('#courseSubmit').replaceWith(`<button id="courseSubmit" onclick="getGolfCourse(${allGolfCourses.courses[i].id})">Choose Your Course</button>`);
             }
         }
     });
@@ -64,6 +60,7 @@ function getGolfCourse(id) {
         if (this.readyState == 4 && this.status == 200) {
             selGolfCourse = JSON.parse(this.responseText);
             objHoleData(selGolfCourse);
+            getPlayers();
             // todo insert modal function call here "select teebox"
             // todo turn off "Select Your Course" display
             // todo draw course card function call drawCard(); courseHoles is globally defined so I don't need to pass it.
@@ -72,6 +69,8 @@ function getGolfCourse(id) {
     xhttp.open("GET", `https://golf-courses-api.herokuapp.com/courses/${id}`, true);
     xhttp.send();
 }
+
+// OBJECTIFICATION HAPPENS HERE XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
 function objHoleData(selGolfCourse) {
     let holes;
@@ -88,6 +87,59 @@ function objHoleData(selGolfCourse) {
         holes = new Holes(h + 1, allTeebox);
         courseHoles.push(holes);
     }
+}
+
+function getPlayers() {
+    let players = [],
+        tee = [],
+        tempNumPlay = 0;
+    $('.containerCourse').empty();
+    $('.playerSetup').append(`<h2>Players Here</h2>`);
+    for (let p = 0; p < 4; p++) {
+        $('.playerSetup').append(`<div id="player${p}"><input type="text" id="${p}" placeholder="Player ${p + 1}" value="one" onkeydown="checkName(this.id, this.value, event)"></div>`);
+    }
+    // PUT SUBMIT HERE
+
+}
+
+function checkName(id, name, event) {
+    switch (event.key) {
+        case 'Tab':
+            console.log(`I got tabbed the id is ${id} and the name is ${name}`);
+            testPlayer.push(name);
+            for (let i = 0; i < testPlayer.length - 1; i++) {
+                console.log(`name = ${name}    testPlayer[${i}] = ${testPlayer[i]}`);
+                if (name === testPlayer[i]) {
+                    // todo put modal here
+                    $(`#player${id}`).replaceWith(`<div id="player${id}"><input type="text" id="${id}" placeholder="Player ${Number(id) + 1}" onkeydown="checkName(this.id, this.value, event)"></div>`);
+                    testPlayer.pop();
+                }
+            }
+            break;
+        case 'Enter':
+            console.log(`I got entered the id is ${id} and the name is ${name}`);
+            testPlayer.push(name);
+            $(`#player${id + 1}`).focus();
+            for (let i = 0; i < testPlayer.length - 1; i++) {
+                console.log(`name = ${name}    testPlayer[${i}] = ${testPlayer[i]}`);
+                if (name === testPlayer[i]) {
+                    // todo put modal here
+                    console.log(`I got here`);
+                    $(`#player${id}`).replaceWith(`<div id="player${id}"><input type="text" id="${id}" placeholder="Player ${Number(id) + 1}" onkeydown="checkName(this.id, this.value, event)"></div>`);
+                    $(`#player${id}`).focus();
+                    testPlayer.pop();
+                }
+            }
+            break;
+    }
+}
+
+function objPlayerData(players) {
+
+
+    // console.log(`numPlayer ${numPlayer}, playerName ${playerName}, teeBox ${teeBox}`);
+
+
 }
 
 // THIS SECTION DRAWS AND SETS UP THE SCORECARD  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
@@ -127,8 +179,4 @@ function drawCard() {
     }
 }
 
-
-
-
 getGolfCourses();
-// drawCard();
